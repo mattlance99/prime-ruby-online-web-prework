13db15f48453ebf40bfbe6b83404a3f2645dbbe8
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-15 23:55:11 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,11 @@
 # Add  code here!
 def prime?(integer)
-  (2..integer - 1).each {|x| return false if (integer % x) == 0 }
-  true
+    return false if integer < 2
+    (2..integer - 1).each do |x|
+        if (integer % x) == 0
+            return false
+        end
+    end
+    true
 end
+
